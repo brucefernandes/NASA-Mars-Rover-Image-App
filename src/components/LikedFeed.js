7fe4d318f8102204d23b1761e0e964909d6fcab8
Bruce Brad Fernandes <brucef300@gmail.com>
2022-01-10 02:12:25 +0400

    Links created

---
+++ src/components/LikedFeed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LikedFeed = () => {
+    return (
+        <div>
+            Liked Feed
+        </div>
+    )
+}
+
+export default LikedFeed
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,45 @@
 import React, {useState} from 'react'
 import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material'
 import '../styles/Navbar.css'
-
-
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import ImageFeed from "./ImageFeed";
+import LikedFeed from './LikedFeed';
 
 const Navbar = () => {
     const [value, setValue] = useState(0)
 
+    const routes = ["/imageFeed", "/likedFeed"]
     const handleOnClickTab = (e, newValue) =>{
         setValue(newValue);
     }
     return (
         <div>
-            <AppBar className="navbar_appbar" position="static" style={{backgroundColor: '#130c4a', boxShadow: "0px 0 px 0px 0px"}}>
-                <Toolbar>
-                    <Typography>NASA Rover Image Feed</Typography>
-                    
-                    <Tabs indicatorColor='secondary' onChange={handleOnClickTab} value={value} textColor='inherit' >
-                        <Tab disableRipple label='Home Feed'/>
-                        <Tab disableRipple label='Liked Images'/>
+            <BrowserRouter>
+                <Route path="/" render={(history) => (
+                    <AppBar className="navbar_appbar" position="static" style={{backgroundColor: '#130c4a', boxShadow: "0px 0 px 0px 0px"}}>
+                        <Toolbar>
+                            <Typography>NASA Rover Image Feed</Typography>
+                            
+                            <Tabs indicatorColor='secondary' onChange={handleOnClickTab} value={history.location.pathname !== "/" ? history.location.pathname : false} textColor='inherit' >
+                                <Tab disableRipple label='Home Feed' component={Link} to={routes[0]}/>
+                                <Tab disableRipple label='Liked Images' component={Link} to={routes[1]} />
 
-                    </Tabs>
+                            </Tabs>
 
-                </Toolbar>
-            </AppBar>
+                        </Toolbar>
+                    </AppBar> 
+
+                )}>
+
+
+                </Route>
+
+                <Switch>
+                    <Route path="/imageFeed" component={ImageFeed}/>
+                    <Route path="/likedFeed" component={LikedFeed}/>
+                </Switch>
+                
+            </BrowserRouter>
         </div>
     )
 }
